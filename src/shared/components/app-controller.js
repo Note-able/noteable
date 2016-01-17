6f999a203a9c514e7a113c55ebbc8285c98730a5
Michael Nakayama <michael.nakayama@WS2206-4.T-mobile.com>
2016-01-16 21:57:21 -0800

    Initial implementation.

--- src/shared/components/app-controller.js
+++ src/shared/components/app-controller.js
@@ -15,17 +15,9 @@ module.exports = class AppController extends React.Component {
   render() {
     return(
       <div>
-        <link href='/css/post.css' rel='stylesheet' type='text/css'/>
         <link href='/css/bundle.css' rel='stylesheet' type='text/css'/>
-        <div id='app-container'>
+        <div className='app-container'>
           <div className="nav-button-container">
-            <div className="nav-button-container__bar">
-              <li id='home' className='nav-button' onclick='window.location.href = "/"'><a href='/'></a></li>
-              <li id='showPosts' className='nav-button' onclick="window.location.href = '/Tech'"><a href='/Tech'></a></li>
-              <li id='showResume' className='nav-button'><a href='/resume'></a></li>
-              <li id='showGallery' className='nav-button'><a href='/photos/gallery'></a></li>
-              <li id='showAdventure' className='nav-button' onclick="window.location.href = '/Adventure'"><a href='/Adventure'></a></li>
-            </div>
           </div>
           { this.props.children }
         </div>
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 var webpack = require('webpack');
 var cleanWebpack = require('clean-webpack-plugin');
 
-var ignore = new webpack.IgnorePlugin(new RegExp("/(node_modules|ckeditor)/"))
-
 module.exports = {
   devtool: 'inline-source-map',
   entry: './lib/client/entry',
@@ -11,24 +9,16 @@ module.exports = {
     filename: 'app.js',
     publicPath: 'http://localhost:8081/js/',
   },
-  plugins: [
-    ignore,
-    new webpack.NoErrorsPlugin(),
-   // new cleanWebpack(['lib'])
-  ],
   resolve: {
-    modulesDirectories: ['node_modules', 'shared'],
-    extensions: ['', '.js']
-  },
-  watchOptions: {
-    poll: true,
+    extensions: ['', '.js'],
+    moduleDirectories: ['./node_modules']
   },
   module: {
     loaders: [
       {
         test: /\.js?$/,
-        loaders: ['react-hot', 'babel-loader?presets[]=react,presets[]=es2015,plugins[]=transform-es2015-classes,plugins[]=transform-react-jsx'],
-        exclude: /(node_modules|ckeditor)/,
+        loaders: ['babel-loader?presets[]=react,presets[]=es2015,plugins[]=transform-es2015-classes,plugins[]=transform-react-jsx'],
+        exclude: /(node_modules)/,
       }
     ]
   }
