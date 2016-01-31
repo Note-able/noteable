cc441632627b95ebe021cc2dcca1708207589749
Ian Mundy <mundycore@gmail.com>
2016-01-30 23:27:04 -0800

    Information flow of Sections and Lines Updated. Arrow keys move lines up and down, but caret offset needs fixed

--- src/shared/components/editor-controller.js
+++ src/shared/components/editor-controller.js
@@ -26,8 +26,9 @@ class Section extends React.Component {
     	super(props, context);
 
     	var lines = 0;
+ 		var lineData = [this.newLine(lines, '')];
 		this.lines = lines;
-		this.state = { lineData: [this.newLine(lines, '')] };
+		this.state = { lineData: lineData, selectedLine: lineData[0] };
 	}
 
 	newLine(id, text) {
@@ -38,21 +39,48 @@ class Section extends React.Component {
 		++this.lines;
 		var lineData = this.state.lineData;
 		var newLine = this.newLine(this.lines, text)
-		if(lineId !== lineData.length) {
+		var selected = this.state.selected;
+		if(lineId !== lineData[lineData.length - 1].lineId) {
 			var index = lineData.findIndex(function(e, i, a) { return e.lineId === lineId });
 			lineData.splice(index + 1, 0, newLine);
+			selected = lineData[index + 1];
 		} else {
 			lineData.push(newLine);
+			selected = lineData[lineData.length - 1];
 		}
-		this.setState({ lineData: lineData });
+		this.setState({ lineData: lineData, selectedLine: selected});
+	}
+
+	handleDelete() {
+
+	}
+
+	handleUpArrow(offset, lineId) {
+		if(this.state.lineData[0].lineId !== lineId){
+			var index = this.state.lineData.findIndex(function(e, i, a) { return e.lineId === lineId });
+			this.setState({ selectedLine: this.state.lineData[index - 1], offset: offset });
+		}
+	}
+
+	handleDownArrow(offset, lineId) {
+		if(this.state.lineData[this.state.lineData.length - 1].lineId !== lineId){
+			var index = this.state.lineData.findIndex(function(e, i, a) { return e.lineId === lineId });
+			this.setState({ selectedLine: this.state.lineData[index + 1], offset: offset });
+		}
 	}
 
 	render() {
 		var lineElements = this.state.lineData.map(function(line){
-			return (<Line key={ line.lineId } lineId={ line.lineId } text={ line.text } handleEnter={ this.handleEnter.bind(this) }></Line>);
+			var selected = line.lineId === this.state.selectedLine.lineId;
+			var offset = selected ? this.state.offset : null;
+			return (<Line key={ line.lineId } lineId={ line.lineId } text={ line.text } selected={ selected } offset={ offset }
+				handleEnter={ this.handleEnter.bind(this) }
+				handleDelete={ this.handleDelete.bind(this) }
+				handleUpArrow={ this.handleUpArrow.bind(this) }
+				handleDownArrow={ this.handleDownArrow.bind(this) }></Line>);
 		}.bind(this));
 		return (
-		<div className="section" name={ this.props.sectionId }>
+		<div className="section" ref="section" name={ this.props.sectionId }>
 			{ lineElements }
 		</div>
 		);
@@ -63,34 +91,44 @@ class Line extends React.Component {
 	constructor(props, context) {
     	super(props, context);
 
-    	this.state = { text: props.text, selected: 'selected' };
+    	var selected = this.props.selected ? "selected" : "";
+    	this.state = { text: this.props.text, selected: selected, caretOffset: 0 };
     	this.enterPressed = false;
     	this.keyMap = [];
 	}
 
 	componentDidMount() {
-		ReactDOM.findDOMNode(this.refs.line).focus(); 
+		if(this.props.selected)
+			ReactDOM.findDOMNode(this.refs.line).focus(); 
 	}
 
 	componentDidUpdate() {
-		if(!this.enterPressed) {
+		if (this.shouldFocus) {
+			ReactDOM.findDOMNode(this.refs.line).focus(); 
+			this.shouldFocus = false;
+		}
+		if(!this.enterPressed && this.isFocused) {
 			this.setCaretPosition.bind(this)();
 		} else {
 			this.enterPressed = false;
 		}
 	}
 
+	componentWillReceiveProps(nextProps) {
+		var selected = nextProps.selected ? "selected" : "";
+		var offset = nextProps.offset == null ? 0 : nextProps.offset;
+		this.shouldFocus = nextProps.selected;
+		this.setState({ selected: selected, caretOffset: offset });
+	}
+
 	setCaretPosition() {
 		var element = ReactDOM.findDOMNode(this.refs.line);
-		if(element.childNodes.length > 0 && this.isFocused && !this.isFocusing)
-		{
+		if(element.childNodes.length !== 0) {
 			var selection = window.getSelection();
 			var range = document.createRange();
 			range.setStart(element.childNodes[0], this.state.caretOffset);
 			selection.removeAllRanges();
 			selection.addRange(range);
-		} else if(this.isFocusing) {
-			this.isFocusing = false;
 		}
 	}
 
@@ -104,23 +142,26 @@ class Line extends React.Component {
 	}
 
 	handleKeyDown(e) {
-		console.log('keydown');
-		console.log(e);
 		this.keyMap[e.keyCode] = e.type == 'keydown';
-		if(e.keyCode === 13)
-		{
+		if(e.keyCode === 13) { //enter
 			this.enterPressed = true;
 			var selectionOffset = window.getSelection().baseOffset;
 			var movedText = this.state.text.substring(selectionOffset, this.state.text.length);
 			var remainingText = this.state.text.substring(0, selectionOffset);
 			this.props.handleEnter(movedText, this.props.lineId);
 			this.setState({ text: remainingText });
+		} else if(e.keyCode === 38) { //upArrow
+			var selectionOffset = window.getSelection().baseOffset;
+			this.props.handleUpArrow(selectionOffset, this.props.lineId);
+		} else if(e.keyCode === 40) { //downArrow
+			var selectionOffset = window.getSelection().baseOffset;
+			this.props.handleDownArrow(selectionOffset, this.props.lineId);
 		}
 	}
 
 	handleKeyUp (e){
 		this.keyMap[e.keyCode] = e.type == 'keydown';
-		if (e.keyCode === 8) {
+		if (e.keyCode === 8) { //delete
 			var newText = ReactDOM.findDOMNode(this.refs.line).innerHTML;
 			this.setState({ text: newText });
 		}
@@ -128,19 +169,19 @@ class Line extends React.Component {
 
 	handleClick(e) {
 		console.log("clicked");
-		var el = ReactDOM.findDOMNode(this.refs.line);
-		el.focus(); 
+		if(this.state.selected === "") {
+			var selectionOffset = window.getSelection().baseOffset;
+			this.setState({ selected: "selected", caretOffset: selectionOffset });
+		}
 	}
 
 	handleOnFocus() {
 		this.isFocused = true;
-	    this.isFocusing = true;
-		this.setState({ selected: 'selected' });
 	}
 
 	handleOnBlur() {
 		this.isFocused = false;
-		this.setState({ selected: '' });
+		this.setState({ selected: "" });
 	}
 
 	render() {
