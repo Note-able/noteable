58507469179760ff9243f238bbd48277a3b23a80
Ian Mundy <mundycore@gmail.com>
2016-02-07 22:52:30 -0800

    Preserving status of first run at text editor

--- src/shared/components/editor-controller.js
+++ src/shared/components/editor-controller.js
@@ -27,50 +27,122 @@ class Section extends React.Component {
 
     this.lines = 0;
     const lineData = [this.newLine (this.lines, '')];
-    this.state = { lineData: lineData, selectedLine: lineData[0] };
+    this.state = { lineData: lineData, selectedLine: lineData[0], selectedLineIndex: 0 };
+    this.enterPressed = false;
+    this.keyMap = [];
   }
 
   newLine (id, text) {
     return { lineId: id, text: text }
   }
 
-  handleEnter (text, lineId) {
+  handleClick () {
+    console.log('clicked');
+  }
+
+  handlePaste (e) {
+    console.log('paste');
+    e.preventDefault();
+  }
+
+  handleKeyPress (e) {
+    e.preventDefault();
+    const charCode = e.which || e.keyCode;
+    const charTyped = String.fromCharCode(charCode);
+    const newText = this.state.selectedLine.text + charTyped;
+    const offset = window.getSelection().focusOffset + 1;
+    const newLine = this.state.selectedLine;
+    newLine.text = newText;
+    const newLineData = this.setTextOfChild.bind(this)(newText);
+    this.setState({ lineData: newLineData, selectedLine: newLine, offset: offset });
+  }
+
+  setTextOfChild (text) {
+    const lineData = this.state.lineData;
+    lineData[this.state.selectedLineIndex].text = text;
+    return lineData;
+  }
+
+  handleKeyDown (e) {
+    this.keyMap[e.keyCode] = e.type === 'keydown';
+    if(e.keyCode === 13) { //enter
+      e.preventDefault();
+      this.enterPressed = true;
+      const selectionOffset = window.getSelection().baseOffset;
+      const movedText = this.state.selectedLine.text.substring(selectionOffset, this.state.selectedLine.text.length);
+      const remainingText = this.state.selectedLine.text.substring(0, selectionOffset);
+      this.handleEnter.bind(this)(remainingText, movedText);
+    } else if(e.keyCode === 38) { //upArrow
+      e.preventDefault();
+      const selectionOffset = window.getSelection().baseOffset;
+      this.handleUpArrow(selectionOffset, this.state.selectedLine.lineId);
+    } else if(e.keyCode === 40) { //downArrow
+      e.preventDefault();
+      const selectionOffset = window.getSelection().baseOffset;
+      this.handleDownArrow(selectionOffset, this.state.selectedLine.lineId);
+    }
+  }
+
+  handleKeyUp (e){
+    this.keyMap[e.keyCode] = e.type === 'keydown';
+    if (e.keyCode === 8) { //delete
+      if(window.getSelection().baseOffset === 0) {
+        this.handleDelete(this.state.selectedLine.text);
+      }
+      const newText = ReactDOM.findDOMNode(this.refs.line).innerHTML;
+      this.setState({ text: newText });
+    }
+  }
+
+  handleOnFocus () {
+    this.isFocused = true;
+  }
+
+  handleOnBlur () {
+    this.isFocused = false;
+  }
+
+  handleEnter (oldText, movedText) {
     ++this.lines;
     const lineData = this.state.lineData;
-    const newLine = this.newLine(this.lines, text)
-    let selected = this.state.selected;
-    if(lineId !== lineData[lineData.length - 1].lineId) {
-      const index = lineData.findIndex((e) => { return e.lineId === lineId });
-      lineData.splice(index + 1, 0, newLine);
-      selected = lineData[index + 1];
+    const newLine = this.newLine(this.lines, movedText)
+    let selectedIndex = this.state.selectedLineIndex;
+    let selected = this.state.selectedLine;
+    if(selectedIndex !== (lineData.length - 1)) {
+      lineData[selectedIndex].text = oldText;
+      lineData.splice(selectedIndex + 1, 0, newLine);
+      selectedIndex = selectedIndex + 1;
+      selected = lineData[selectedIndex];
     } else {
+      lineData[lineData.length - 1].text = oldText;
       lineData.push(newLine);
-      selected = lineData[lineData.length - 1];
+      selectedIndex = lineData.length - 1
+      selected = lineData[selectedIndex];
     }
-    this.setState({ lineData: lineData, selectedLine: selected});
+    this.setState({ lineData: lineData, selectedLineIndex: selectedIndex, selectedLine: selected });
   }
 
   handleDelete () {
-
+    console.log('delete');
   }
 
   handleUpArrow (offset, lineId) {
     if(this.state.lineData[0].lineId !== lineId){
       const index = this.state.lineData.findIndex((e) => { return e.lineId === lineId });
-      this.setState({ selectedLine: this.state.lineData[index - 1], offset: offset });
+      this.setState({ selectedLineIndex: index - 1, offset: offset });
     }
   }
 
   handleDownArrow (offset, lineId) {
     if(this.state.lineData[this.state.lineData.length - 1].lineId !== lineId){
       const index = this.state.lineData.findIndex((e) => { return e.lineId === lineId });
-      this.setState({ selectedLine: this.state.lineData[index + 1], offset: offset });
+      this.setState({ selectedLineIndex: index + 1, offset: offset });
     }
   }
 
   render () {
     const lineElements = this.state.lineData.map((line) => {
-      const selected = line.lineId === this.state.selectedLine.lineId;
+      const selected = line.lineId === this.state.lineData[this.state.selectedLineIndex].lineId;
       const offset = selected ? this.state.offset : null;
       return (<Line key={ line.lineId } lineId={ line.lineId } text={ line.text } selected={ selected } offset={ offset }
         handleEnter={ this.handleEnter.bind(this) }
@@ -79,7 +151,14 @@ class Section extends React.Component {
         handleDownArrow={ this.handleDownArrow.bind(this) }></Line>);
     });
     return (
-    <div className="section" ref="section" name={ this.props.sectionId }>
+    <div className="section" ref="section" name={ this.props.sectionId } contentEditable="true"
+      onPaste={ this.handlePaste.bind(this) }
+      onKeyPress={ this.handleKeyPress.bind(this) }
+      onKeyDown={ this.handleKeyDown.bind(this) }
+      onKeyUp={ this.handleKeyUp.bind(this) }
+      onClick={ this.handleClick.bind(this) }
+      onFocus={ this.handleOnFocus.bind(this) }
+      onBlur={ this.handleOnBlur.bind(this) }>
       { lineElements }
     </div>
     );
@@ -102,7 +181,7 @@ class Line extends React.Component {
     }
   }
 
-  componentDidUpdate () {
+  /*componentDidUpdate () {
     if (this.shouldFocus) {
       ReactDOM.findDOMNode(this.refs.line).focus();
       this.shouldFocus = false;
@@ -112,6 +191,9 @@ class Line extends React.Component {
     } else {
       this.enterPressed = false;
     }
+  }*/
+  componentDidUpdate () {
+    this.setCaretPosition.bind(this)();
   }
 
   componentWillReceiveProps (nextProps) {
@@ -125,7 +207,7 @@ class Line extends React.Component {
 
   setCaretPosition () {
     const element = ReactDOM.findDOMNode(this.refs.line);
-    if(element.childNodes.length !== 0 && this.isFocused) {
+    if(element.childNodes.length !== 0 && this.props.selected) {
       const caretPos = this.state.caretOffset > element.firstChild.length ? element.firstChild.length : this.state.caretOffset;
       const selection = window.getSelection();
       const range = document.createRange();
@@ -133,49 +215,6 @@ class Line extends React.Component {
       range.setEnd(element.firstChild, caretPos);
       selection.removeAllRanges();
       selection.addRange(range);
-    }
-  }
-
-  handlePaste (e) {
-    console.log('paste');
-    e.preventDefault();
-  }
-
-  handleKeyPress (e) {
-    e.preventDefault();
-    const charCode = e.which || e.keyCode;
-    const charTyped = String.fromCharCode(charCode);
-    const newText = this.state.text + charTyped;
-    const offset = window.getSelection().focusOffset + 1;
-    this.setState({ text: newText, caretOffset: offset });
-  }
-
-  handleKeyDown (e) {
-    this.keyMap[e.keyCode] = e.type === 'keydown';
-    if(e.keyCode === 13) { //enter
-      e.preventDefault();
-      this.enterPressed = true;
-      const selectionOffset = window.getSelection().baseOffset;
-      const movedText = this.state.text.substring(selectionOffset, this.state.text.length);
-      const remainingText = this.state.text.substring(0, selectionOffset);
-      this.props.handleEnter(movedText, this.props.lineId);
-      this.setState({ text: remainingText });
-    } else if(e.keyCode === 38) { //upArrow
-      e.preventDefault();
-      const selectionOffset = window.getSelection().baseOffset;
-      this.props.handleUpArrow(selectionOffset, this.props.lineId);
-    } else if(e.keyCode === 40) { //downArrow
-      e.preventDefault();
-      const selectionOffset = window.getSelection().baseOffset;
-      this.props.handleDownArrow(selectionOffset, this.props.lineId);
-    }
-  }
-
-  handleKeyUp (e){
-    this.keyMap[e.keyCode] = e.type === 'keydown';
-    if (e.keyCode === 8) { //delete
-      const newText = ReactDOM.findDOMNode(this.refs.line).innerHTML;
-      this.setState({ text: newText });
     }
   }
 
@@ -187,31 +226,14 @@ class Line extends React.Component {
     }
   }
 
-  handleOnFocus () {
-    this.isFocused = true;
-  }
-
-  handleOnBlur () {
-    this.isFocused = false;
-    this.setState({ selected: '' });
-  }
-
   render () {
     return (
-      <p
-      className={ `editor-line { this.state.selected }` }
+      <div
+      className={ `editor-line ${ this.state.selected }` }
       name={ this.props.lineId }
-      ref="line"
-      onPaste={ this.handlePaste.bind(this) }
-      onKeyPress={ this.handleKeyPress.bind(this) }
-      onKeyDown={ this.handleKeyDown.bind(this) }
-      onKeyUp={ this.handleKeyUp.bind(this) }
-      onClick={ this.handleClick.bind(this) }
-      onFocus={ this.handleOnFocus.bind(this) }
-      onBlur={ this.handleOnBlur.bind(this) }
-      contentEditable="true">
-        { this.state.text}
-      </p>
+      ref="line">
+        { this.props.text }
+      </div>
     );
   }
 }
