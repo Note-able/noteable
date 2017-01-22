11900dc7ca3b19dbefe06871600c7781fc09c0ca
Ian Mundy <mundycore@gmail.com>
2017-01-21 18:04:39 -0800

    Fix id in conversationDto

--- src/server/services/messageService/model/conversationDto.js
+++ src/server/services/messageService/model/conversationDto.js
@@ -14,11 +14,11 @@ export const conversationMapper = (users, conversation) => {
 
 export const conversationsMapper = (conversations) => {
   const conversationMap = conversations.reduce((map, conversation) => {
-    const { conversation_id, message_id, message_content, message_user_id, ...user } = conversation;
-    if (map[conversation_id])
-      map[conversation_id].users.push(user);
+    const { conversationid, message_id, message_content, message_user_id, ...user } = conversation;
+    if (map[conversationid])
+      map[conversationid].users.push(user);
     else
-      map[conversation_id] = { users: [user], lastMessage: { id: message_id, content: message_content, userId: message_user_id } };
+      map[conversationid] = { users: [user], lastMessage: { id: message_id, content: message_content, userId: message_user_id } };
     return map;
   }, {});
   return Object.keys(conversationMap).map((id) => {
